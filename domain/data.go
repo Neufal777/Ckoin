00c896603536bd2db7d65d4866af0fbd71a0b67a
neufal777 <neufal777@gmail.com>
2020-04-01 22:56:21 +0200

    test

--- domain/data.go
+++ domain/data.go
@@ -2,23 +2,49 @@ package domain
 
 import (
 	"fmt"
-	"io/ioutil"
-	"log"
 	"net/http"
 )
 
+type PriceBtc struct {
+	High      float32
+	Last      float32
+	Timestamp int64
+	Bid       float32
+	Vwap      float32
+	Volume    float32
+	Low       float32
+	Ask       float32
+	Open      float32
+}
+
 func Pricebtc(w http.ResponseWriter, r *http.Request) {
-	resp, err := http.Get("https://www.bitstamp.net/api/ticker/")
 
-	if err != nil {
-		log.Println(err)
+	example := &PriceBtc{
+		High:      6511.60000000,
+		Last:      6198.04,
+		Timestamp: 1585760373,
+		Bid:       6191.84,
+		Vwap:      6312.49,
+		Volume:    6685.93951412,
+		Low:       6137.71000000,
+		Ask:       6198.04,
+		Open:      6428.74,
 	}
-	defer resp.Body.Close()
-	body, _ := ioutil.ReadAll(resp.Body)
 
-	defer resp.Body.Close()
-	//jsonData, err := json.MarshalIndent(body, "", "    ")
+	// resp, err := http.Get("https://www.bitstamp.net/api/ticker/")
 
-	res := string(body)
-	fmt.Fprintf(w, res)
+	// if err != nil {
+	// 	log.Println(err)
+	// }
+	// defer resp.Body.Close()
+	// body, _ := ioutil.ReadAll(resp.Body)
+
+	// defer resp.Body.Close()
+	// //jsonData, err := json.MarshalIndent(body, "", "    ")
+
+	// res := string(body)
+
+	i := example.Last
+	x := fmt.Sprintf("%f", i)
+	fmt.Fprintf(w, x)
 }
--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"log"
 	"net/http"
 
 	domain "github.com/ckoin/domain"
@@ -8,6 +9,19 @@ import (
 
 func main() {
 
+	info := domain.PriceBtc{
+		High:      6511.60000000,
+		Last:      6198.04,
+		Timestamp: 1585760373,
+		Bid:       6191.84,
+		Vwap:      6312.49,
+		Volume:    6685.93951412,
+		Low:       6137.71000000,
+		Ask:       6198.04,
+		Open:      6428.74,
+	}
+
+	log.Println(info)
 	http.HandleFunc("/", domain.Pricebtc)
 	http.ListenAndServe(":8000", nil)
 }
