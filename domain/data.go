9091f5ba176bd6b4ed437071f8117df3e0e15e26
neufal777 <neufal777@gmail.com>
2020-04-01 19:05:13 +0200

    saving changes

--- domain/data.go
+++ domain/data.go
@@ -1,8 +1,24 @@
 package domain
 
-import "log"
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
 
-func Base() {
+func Pricebtc(w http.ResponseWriter, r *http.Request) {
+	resp, err := http.Get("https://www.bitstamp.net/api/ticker/")
 
-	log.Println("Base")
+	if err != nil {
+		log.Println(err)
+	}
+	defer resp.Body.Close()
+	body, _ := ioutil.ReadAll(resp.Body)
+
+	defer resp.Body.Close()
+	//jsonData, err := json.MarshalIndent(body, "", "    ")
+
+	res := string(body)
+	fmt.Fprintf(w, res)
 }
--- main.go
+++ main.go
@@ -1,26 +1,13 @@
 package main
 
 import (
-	"fmt"
-	"io/ioutil"
-	"log"
 	"net/http"
+
+	domain "github.com/ckoin/domain"
 )
 
 func main() {
 
-	resp, err := http.Get("https://www.bitstamp.net/api/ticker/")
-
-	if err != nil {
-		log.Println(err)
-	}
-	defer resp.Body.Close()
-	body, _ := ioutil.ReadAll(resp.Body)
-
-	defer resp.Body.Close()
-	//jsonData, err := json.MarshalIndent(body, "", "    ")
-
-	res := string(body)
-
-	fmt.Println(res)
+	http.HandleFunc("/", domain.Pricebtc)
+	http.ListenAndServe(":8000", nil)
 }
