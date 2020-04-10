ec8be4095b0852adb82d4b60503a2b43995a14c0
neufal777 <neufal777@gmail.com>
2020-04-10 16:55:24 +0200

    working

--- app/core.go
+++ app/core.go
@@ -8,19 +8,7 @@ import (
 	"strings"
 )
 
-type PriceCoin struct {
-	High      float32
-	Last      float32
-	Timestamp int64
-	Bid       float32
-	Vwap      float32
-	Volume    float32
-	Low       float32
-	Ask       float32
-	Open      float32
-}
-
-func Home() {
+func CheckPrices() {
 
 	cryptos := []string{
 		"btcusd",
@@ -41,15 +29,15 @@ func Home() {
 	}
 
 	for _, c := range cryptos {
+
 		resp, err := http.Get("https://www.bitstamp.net/api/v2/ticker/" + c + "/")
 
 		if err != nil {
 			log.Println(err)
 		}
-		defer resp.Body.Close()
-		body, _ := ioutil.ReadAll(resp.Body)
 
 		defer resp.Body.Close()
+		body, _ := ioutil.ReadAll(resp.Body)
 
 		res := string(body)
 
